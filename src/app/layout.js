f69cb6de5efa32c72f86da2ce705f307e8574583
tuhinhossain123 <csetuhin55@gmail.com>
2024-11-28 19:19:12 +0600

    project setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#000] max-w-[858px] mx-auto`}
       >
         {children}
       </body>
---
+++ src/components/Category/AllCategory/AllCategory.js
@@ -0,0 +1,16 @@
+const AllCategory = () => {
+  return (
+    <div className="mt-[102px] flex justify-between">
+      <div>
+        <h2>link</h2>
+      </div>
+      <div>
+        <button className="text-lg font-normal px-5 py-[14px]">
+          Add Animal
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AllCategory;
